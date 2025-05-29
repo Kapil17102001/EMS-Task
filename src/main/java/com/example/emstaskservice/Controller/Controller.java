540dddf4dfeb1e8e743681f48065c9c6a9ca898f
Kapil Dev Saini <saini17102001@gmail.com>
2025-05-29 11:20:47 +0530

    Route To Add New Task

---
+++ src/main/java/com/example/emstaskservice/Controller/Controller.java
@@ -0,0 +1,40 @@
+package com.example.emstaskservice.controller;
+
+import com.example.emstaskservice.OpenFeign.Validate;
+import com.example.emstaskservice.dto.*;
+import com.example.emstaskservice.enums.Priority;
+import com.example.emstaskservice.enums.TaskStatus;
+import com.example.emstaskservice.exception.CustomException;
+import com.example.emstaskservice.model.TaskModel;
+import com.example.emstaskservice.service.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+public class Controller {
+    private final TaskService taskService;
+    @Autowired
+    public Controller(TaskService taskService){
+        this.taskService = taskService;
+    }
+    @Autowired
+    private Validate validate;
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<ResponseDto> add (@Valid @RequestBody RequestInsertTaskDto requestInsertTaskDto)
+    {
+       return taskService.addTask(requestInsertTaskDto);
+
+    }
+
+
+
+}
